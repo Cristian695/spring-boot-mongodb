61ee4d0fedae8901c09c82d35b6697afd7229e1e
Cristian <cristiansf695@hotmail.com>
2022-12-12 10:51:28 -0300

    user insert with post

--- src/main/java/com/cristian695/workshopmongo/Services/UserService.java
+++ src/main/java/com/cristian695/workshopmongo/Services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.cristian695.workshopmongo.Services.exception.ObjectNotFoundException;
 import com.cristian695.workshopmongo.domain.User;
+import com.cristian695.workshopmongo.dto.UserDTO;
 import com.cristian695.workshopmongo.repository.UserRepository;
 
 @Service
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }

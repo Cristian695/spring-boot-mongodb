9bd4545b3baee0adf8e497972d97255c42d4d8ed
Cristian <cristiansf695@hotmail.com>
2022-12-19 10:12:50 -0300

    Author with DTO

--- src/main/java/com/cristian695/workshopmongo/domain/Post.java
+++ src/main/java/com/cristian695/workshopmongo/domain/Post.java
@@ -7,6 +7,8 @@ import java.util.Objects;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.cristian695.workshopmongo.dto.AuthorDTO;
+
 @Document
 public class Post implements Serializable{
 	private static final long serialVersionUID = 1L;
@@ -16,12 +18,12 @@ public class Post implements Serializable{
 	private Date date;
 	private String title;
 	private String body;
-	private User Author;
+	private AuthorDTO Author;
 	
 	public Post() {
 	}
 
-	public Post(String id, Date date, String title, String body, User Author) {
+	public Post(String id, Date date, String title, String body, AuthorDTO Author) {
 		super();
 		this.id = id;
 		this.date = date;
@@ -62,11 +64,11 @@ public class Post implements Serializable{
 		this.body = body;
 	}
 
-	public User getAuthor() {
+	public AuthorDTO getAuthor() {
 		return Author;
 	}
 
-	public void setAuthor(User author) {
+	public void setAuthor(AuthorDTO author) {
 		Author = author;
 	}
 

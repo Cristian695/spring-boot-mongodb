6cea6f545bc52e2b2a8a2bf324b917b4a3f90d93
Cristian <cristiansf695@hotmail.com>
2022-12-19 10:30:33 -0300

    getting post by id

---
+++ src/main/java/com/cristian695/workshopmongo/Services/PostService.java
@@ -0,0 +1,23 @@
+package com.cristian695.workshopmongo.Services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cristian695.workshopmongo.Services.exception.ObjectNotFoundException;
+import com.cristian695.workshopmongo.domain.Post;
+import com.cristian695.workshopmongo.repository.PostRepository;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
